573c7479cee37afaa3fb0fb07d6d955751596878
Dinar Saberov <dinarik175@yandex.ru>
2024-10-10 10:35:22 +0300

    first commit

--- src/datasets/librispeech_dataset.py
+++ src/datasets/librispeech_dataset.py
@@ -72,6 +72,7 @@ class LibrispeechDataset(BaseDataset):
     def _create_index(self, part):
         index = []
         split_dir = self._data_dir / part
+        print(split_dir)
         if not split_dir.exists():
             self._load_part(part)
 
